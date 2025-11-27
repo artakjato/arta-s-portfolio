bcd8caf7bd72f4436fdf259aa7c9411035d3a7a6
Arta Kjato <artakjato@gmail.com>
2025-11-27 12:33:07 +0100

    add styling to TechSection

--- src/components/TechSection.jsx
+++ src/components/TechSection.jsx
@@ -1,32 +1,43 @@
 import styled from "styled-components";
 
+const Section = styled.section`
+  text-align: center;
+`;
+
+const Heading = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+`;
+
+const TechList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 1.5rem auto 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 0.36rem 1rem;
+  max-width: 40rem;
+`;
+
+const TechItem = styled.li`
+  font-size: 1rem;
+  color: #111827;
+`;
+
 function TechSection () {
   return (
     <Section id="tech">
       <Heading>Tech</Heading>
-      <Text>
-        HTML • CSS • Flexbox • Responsive Design • JavaScript (ES6)
-        JSX • React • React Hooks • Web Accessibility
-        Node.js • MongoDB • APIs
-        GitHub • Pair Programming
-      </Text>
       <TechList>
-        <TechItem>HTML</TechItem>
-        <TechItem>CSS</TechItem>
-        <TechItem>Flexbox</TechItem>
-        <TechItem>Responsive Design</TechItem>
-        <TechItem>JavaScript (ES6)</TechItem>
-        <TechItem>JSX</TechItem>
-        <TechItem>React</TechItem>
-        <TechItem>React Hooks</TechItem>
-        <TechItem>Web Accessibility</TechItem>
-        <TechItem>Node.js</TechItem>
-        <TechItem>MongoDB</TechItem>
-        <TechItem>APIs</TechItem>
-        <TechItem>GitHub</TechItem>
-        <TechItem>Pair Programming</TechItem>
+        <TechItem>HTML • CSS • Flexbox • Responsive Design • JavaScript (ES6)</TechItem>
+        <TechItem>JSX • React • React Hooks • Web Accessibility</TechItem>
+        <TechItem>Node.js • MongoDB • APIs</TechItem>
+        <TechItem>GitHub • Pair Programming</TechItem>
 
       </TechList>
     </Section>
   );
 }
+
+export default TechSection; 
